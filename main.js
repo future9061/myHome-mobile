91a53dcf2177ec9a9d97cbe2efe1b0132c19c10b
future9061 <132829711+future9061@users.noreply.github.com>
2023-06-09 00:04:50 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const slideWrap = document.querySelector(".slide_wrap"); //container
 const slideUl = document.querySelector(".slide_wrap ul"); //ul
 const slideLi = document.querySelectorAll(".slide_wrap ul li");
 const slideUlWidth = slideWrap.offsetWidth * slideLi.length;
+let totalValue = 0;
+
+let slideUlMargin1 = parseInt(window.getComputedStyle(slideUl).marginLeft);
+let slideUlMargin2 = slideLi[0].offsetWidth;
+//slideUlMargin2에 해당 li의 length 곱해서 marginLeft 값 지정
+console.log(slideUlMargin1, slideUlMargin2 * 1);
 
 slideUl.style.width = `${slideUlWidth}px`;
 
@@ -17,34 +23,39 @@ let mouseMoveValue = 0;
 const slideLiWid = slideLi[0].offsetWidth;
 let userClick = false;
 
-//for문으로 돌리는데...
-slideLi[0].addEventListener("mousedown", function (e) {
-  mouseDownValue = e.clientX;
-  userClick = true;
-});
+//for문으로 돌리는데...li index 1의 ml값 index2의 ml값 구해놓고if 문으로 넣기
+//조건 let num0과 li 인덱스가 같으면 넘기기
 
-slideLi[0].addEventListener("mousemove", function (e) {
-  let mouseMoveValue = e.clientX;
-  let totalValue = mouseMoveValue - mouseDownValue;
+slideLi.forEach((elem, index) => {
+  elem.addEventListener("mousedown", function (e) {
+    mouseDownValue = e.clientX;
+    userClick = true;
+  });
 
-  if (userClick === true) {
-    let slideUlMargin = parseInt(window.getComputedStyle(slideUl).marginLeft);
+  elem.addEventListener("mousemove", function (e) {
+    let mouseMoveValue = e.clientX;
+    totalValue = mouseMoveValue - mouseDownValue; //??px 이상 움직이면 조건
+    let currentImg = 0;
 
-    if (slideUlMargin <= 0 && totalValue < 0) {
-      slideUl.style.marginLeft = `${totalValue}px`;
+    setTimeout(function () {
+      if (userClick === true) {
+        if (slideUlMargin1 <= 0 && totalValue < 0) {
+          slideUl.style.marginLeft = `${totalValue}px`;
+        }
+      }
+    }, 500);
+  });
+
+  //3.mouseup
+  slideLi[index].addEventListener("mouseup", function (e) {
+    userClick = false;
+
+    if (totalValue < -100) {
+      slideUl.style.marginLeft = `-${slideLi[index].offsetWidth}px`;
+    } else {
+      slideUl.style.marginLeft = `0`;
     }
-  }
-});
-
-//3.mouseup
-slideLi[0].addEventListener("mouseup", function () {
-  userClick = false;
-
-  if (mouseDownValue - mouseMoveValue > slideLiWid * 0.4) {
-    slideUl.style.marginLeft = `-${slideLi[0].offsetWidth}px`;
-  } else {
-    //40%가 안되면 .. 움직이지마
-  }
+  });
 });
 
 //touch

c0c813fead6e9ede1a8661e34fc65e256cf35f49
future9061 <132829711+future9061@users.noreply.github.com>
2023-06-13 23:53:46 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*1. slide */
+/*1. swiper slide */
 
 const slideWrap = document.querySelector(".slide_wrap"); //container
 const slideUl = document.querySelector(".slide_wrap ul"); //ul
@@ -18,60 +18,67 @@ let mouseMoveValue = 0;
 const slideLiWid = slideLi[0].offsetWidth;
 let userClick = false;
 
-//for문으로 돌리는데...li index 1의 ml값 index2의 ml값 구해놓고if 문으로 넣기
-//조건 let num0과 li 인덱스가 같으면 넘기기
+slideLi[0].addEventListener("mousedown", function (e) {
+  mouseDownValue = e.clientX;
+  userClick = true;
+});
 
-slideLi.forEach((elem, index) => {
-  elem.addEventListener("mousedown", function (e) {
-    mouseDownValue = e.clientX;
-    userClick = true;
-  });
+slideUlMarginLeft = parseInt(window.getComputedStyle(slideUl).marginLeft); //0
 
-  let slideUlMargin1 = parseInt(window.getComputedStyle(slideUl).marginLeft);
-  let slideUlMargin2 = slideLi[0].offsetWidth;
-  //slideUlMargin2에 해당 li의 length 곱해서 marginLeft 값 지
-
-  elem.addEventListener("mousemove", function (e) {
+slideLi[0].addEventListener(
+  "mousemove",
+  function (e) {
     let mouseMoveValue = e.clientX;
-    3;
-    totalValue = mouseMoveValue - mouseDownValue; //??px 이상 움직이면 조건
+    totalValue = mouseMoveValue - mouseDownValue;
+    console.log(userClick);
 
     setTimeout(function () {
-      if (userClick === true) {
-        if (slideUlMargin1 <= 0 && totalValue < 0) {
+      if (userClick == true) {
+        if (slideUlMarginLeft == 0 && totalValue <= 0) {
           slideUl.style.marginLeft = `${totalValue}px`;
         }
       }
-    }, 500);
-  });
-  1;
-  let currentImg = 0;
-
-  //3.mouseup
-  slideLi[index].addEventListener("mouseup", function (e) {
-    userClick = false;
-
-    if (totalValue < -100) {
-      let marginLeft = 0;
-      if (currentImg === 1) {
-        marginLeft -= slideUlMargin2;
-        console.log(marginLeft);
-        slideUl.style.marginLeft = `${marginLeft}px`;
-        currentImg += 1;
+      if (userClick == false) {
+        slideUl.style.marginLeft = `0px`;
       }
+    });
+  },
+  300
+);
+//3.mouseup li의 40%이상 당겼으면 넘어가기
 
-      if (currentImg === 2) {
-        // marginLeft -= slideUlMargin2;
-        // slideUl.style.marginLeft = `${marginLeft}px`;
-        // currentImg += 1;
-      } else {
-        console.log("?");
-      }
-    } else {
-      slideUl.style.marginLeft = `0px`;
-    }
-  });
+let slideWrapWidth = slideWrap.offsetWidth;
+
+slideLi[0].addEventListener("mouseup", function (e) {
+  userClick = false;
+
+  // if (slideWrapWidth * 0.4 < totalValue) {
+  //   console.log("?");
+  // } else {
+  // }
 });
+
+//     if (totalValue < -100) {
+//       let marginLeft = 0;
+//       if (currentImg === 1) {
+//         marginLeft -= slideUlMargin2;
+//         console.log(marginLeft);
+//         slideUl.style.marginLeft = `${marginLeft}px`;
+//         currentImg += 1;
+//       }
+
+//       if (currentImg === 2) {
+//         // marginLeft -= slideUlMargin2;
+//         // slideUl.style.marginLeft = `${marginLeft}px`;
+//         // currentImg += 1;
+//       } else {
+//         console.log("?");
+//       }
+//     } else {
+//       slideUl.style.marginLeft = `0px`;
+//     }
+//   });
+// });
 
 //touch
 
@@ -102,3 +109,49 @@ slideLi.forEach((elem, index) => {
 //     //40%가 안되면 .. 움직이지마
 //   }
 // });
+
+//dott slide
+
+let dott = document.querySelectorAll(".dott_wrap li");
+
+dott.forEach((elem, index) => {
+  elem.addEventListener("click", function (e) {
+    dott.forEach((item) => {
+      item.classList.remove("color_change");
+    });
+
+    e.target.classList.add("color_change");
+
+    let marginValue = -(slideWrap.offsetWidth * index) + "px";
+    slideUl.style.marginLeft = `${marginValue}`;
+  });
+});
+
+const content2ItemWrap = document.querySelector(".contant2_item_wrap");
+fetch("./data/product.json")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.length);
+
+    let copyData = [...data];
+
+    copyData.forEach(function (elem, index) {
+      let divEl = document.createElement("div");
+      divEl.classList.add("contant2_item");
+
+      divEl.innerHTML = `
+      <img src="${elem.img}" alt="item-img" />
+      <i class="fa-regular fa-heart color"></i>
+      <div class="contant2_item_text">
+       <h3>${elem.title}</h3>
+       <p>${elem.room}</p>
+       <small class="contant2_item_infor">고층, 15평, 관리비 7만</small>
+     </div>
+      `;
+
+      content2ItemWrap.append(divEl);
+    });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
